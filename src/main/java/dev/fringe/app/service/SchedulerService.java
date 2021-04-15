dc2de982b1deb56b0c425d915cf041d40503c22f
DESKTOP-5NM1579\livin <dev.fringe@gmail.com>
2021-04-15 17:24:16 +0900

    no message

--- src/main/java/dev/fringe/app/service/SchedulerService.java
+++ src/main/java/dev/fringe/app/service/SchedulerService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import dev.fringe.app.String;
 import dev.fringe.app.model.Candles;
 import dev.fringe.app.model.CandlesId;
 import lombok.SneakyThrows;
